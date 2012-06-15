59c7f90f2ab367830e3b006a3f1b498f62911bb2
Anupam Kapoor <anupam.kapoor@gmail.com>
2012-06-15 09:56:39 +0530

    remove the tool-bar (quite distracting) and use terminus as the default font.

--- custom.el
+++ custom.el
@@ -7,10 +7,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
+ '(fringe-mode 0 nil (fringe))
  '(show-paren-mode t)
  '(tool-bar-mode nil)
  '(menu-bar-mode nil)
- '(fringe-mode 0 nil (fringe))
  )
 
 (custom-set-faces
@@ -18,9 +18,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Terminus" :foundry "unknown" :slant normal :weight normal :height 75 :width normal))))
  '(font-lock-keyword-face ((t (:weight normal))))
  '(font-lock-type-face ((t (:weight normal))))
  '(hl-line ((t (:inherit highlight :background "gray27"))))
- '(show-paren-match ((t (:background "gray27" :foreground "#dcdccc" :weight normal))))
- '(default ((t (:family "Liberation Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal))))
- )
+ '(show-paren-match ((t (:background "gray27" :foreground "#dcdccc" :weight normal)))))
