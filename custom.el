18aad4ecf6675d0eaed89a66579c724b989763e1
Anupam Kapoor <anupam.kapoor@gmail.com>
2012-09-21 19:21:15 +0530

    terminus is a better font than anything else

--- custom.el
+++ custom.el
@@ -36,7 +36,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Monaco" :foundry "unknown" :slant normal :weight normal :height 113 :width normal))))
+ '(default ((t (:family "Terminus" :foundry "unknown" :slant normal :weight normal :height 90 :width normal))))
  '(font-lock-keyword-face ((t (:weight normal))))
  '(font-lock-type-face ((t (:weight normal))))
  '(highline-face ((t (:background "gray30"))))
