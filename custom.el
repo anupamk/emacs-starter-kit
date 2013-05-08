b6b4ed867597e3021bb82a92b8cbcae576976c29
anupam kapoor <anupam.kapoor@gmail.com>
2013-05-08 23:53:32 +0530

    darwin specific customizations, mostly related to font handling and minor keyboard mappings

--- custom.el
+++ custom.el
@@ -42,7 +42,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Terminus" :foundry "xos4" :slant normal :weight normal :height 90 :width normal))))
+ '(default ((t (:family "Terminus" :foundry "xos4" :slant normal :weight normal :height 160 :width normal))))
  '(bbdb-company ((t (:slant normal))) t)
  '(bold ((t nil)))
  '(compilation-error ((t (:inherit error :foreground "orange red" :weight normal))))
