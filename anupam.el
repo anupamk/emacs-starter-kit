b3a78f6be70568ca876ecae12cc660b0c2ff535d
Anupam Kapoor <anupam.kapoor@gmail.com>
2013-09-18 08:38:55 +0530

    minimize the usage of custom stuff in themes

--- anupam.el
+++ anupam.el
@@ -10,19 +10,24 @@
 (add-to-list 'load-path (concat dotfiles-dir "/3rdparty/color-theme"))
 (require 'color-theme)
 (color-theme-initialize)
-(color-theme-xemacs)
+
+(add-to-list 'load-path (concat dotfiles-dir "/3rdparty/tomorrow-night"))
+(require 'color-theme-tomorrow)
+
+
+;; (color-theme-blue-mood)
+(color-theme-tomorrow-night-eighties)
 
 ;; setup custom stuff here so that my settings override theme specific stuff
 (when window-system
   (setq frame-title-format '(buffer-file-name "%f" ("%b"))
-	default-cursor-type 'hbar
 	default-frame-alist '(
 			      ;; no fringes
 			      (left-fringe  . 0)
 			      (right-fringe . 0)
 
-			      (background-color . "gray65")
-			      (cursor-color . "gold")
+			      ;; (background-color . "gray55")
+			      ;; (cursor-color . "green yellow")
 
 			      ;; add more frame-specific stuff here
 			      )
