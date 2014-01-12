22cfa582e2a879d1bb8d3c5dc491b70b44f1a1ac
Anupam Kapoor <anupam.kapoor@gmail.com>
2014-01-12 14:53:29 +0530

    dired-x is available by default

--- anupamk/dired.el
+++ anupamk/dired.el
@@ -1,28 +1,23 @@
 ;;
 ;; dired customizations
 ;; 
-(add-hook 'dired-load-hook
-	  (lambda ()
-	    (load "dired-x")
-	    ;;; global variables here
-	    ))
+(require 'dired-x)
 
 ;;
 ;; omit un-interesting files
 ;;
 (add-hook 'dired-mode-hook
           (lambda ()
-            ; omit uninteresting files in all dired buffers
-            (dired-omit-mode t)
-
             ; human readable sizes, and files sorted numerically
             (setq-default dired-listing-switches "-aGghlrt --group-directories-first --time-style=+")
 
             ; stop prompting for recursive copies
             (setq dired-recursive-copies 'always)
 
-            
-            (setq dired-omit-files (concat dired-omit-files "^#\\|\\.$|\\.beam|\\.pyc|\\.o|\\.a|\\.so"
+   	    ;; Enable toggling of uninteresting files.
+            (setq-default dired-omit-mode-p t)
+            (dired-omit-mode t)
+            (setq dired-omit-files (concat dired-omit-files "^#\\|\\.$|\\.beam|\\.pyc|\\.o|\\.a|\\.so|\\.*~"
                                            ))
             ))
 
