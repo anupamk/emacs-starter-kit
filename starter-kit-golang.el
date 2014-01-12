f5ee8af6245df92c67b388053ad4ffc3ed00edf4
Anupam Kapoor <anupam.kapoor@gmail.com>
2014-01-12 14:51:01 +0530

    more programming-mode friendly stuff. should all go to a common place.

--- starter-kit-golang.el
+++ starter-kit-golang.el
@@ -3,6 +3,18 @@
 
 (require 'go-mode-load)
 
+(defun local-column-number-mode ()
+  (make-local-variable 'column-number-mode)
+  (column-number-mode t))
+
+(defun local-comment-auto-fill ()
+  (set (make-local-variable 'comment-auto-fill-only-comments) t)
+  (auto-fill-mode t))
+
+(defun turn-on-save-place-mode	() (setq save-place t))
+(defun turn-on-whitespace	() (whitespace-mode t))
+(defun turn-on-paredit		() (paredit-mode t))
+
 ;; on compilation, 'go run' the current buffer
 (add-hook 'go-mode-hook
           (lambda ()
@@ -14,10 +26,16 @@
             (unless (file-exists-p "Makefile")
               (set (make-local-variable 'compile-command)
                    (let ((file (file-name-nondirectory buffer-file-name)))
-                     (concat "go build -o obj/" (file-name-sans-extension file) " "file))))))
+                     (concat "go build -o obj/" (file-name-sans-extension file) " "file))))
+	    ))
 
 ;; run 'gofmt' on the file before saving.
 (add-hook 'before-save-hook 'gofmt-before-save)
+
+(add-hook 'go-mode-hook 'local-column-number-mode)
+(add-hook 'go-mode-hook 'local-comment-auto-fill)
+(add-hook 'go-mode-hook 'turn-on-save-place-mode)
+
 
 ;;; ----------------------------------------------------------------
 ;;; starter-kit-golang ends here
