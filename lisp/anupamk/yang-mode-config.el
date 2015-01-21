3ce9b43f78f4c96417b1146a8457c55747bfebe6
Parallel Wireless <parallel@centos-dev.(none)>
2015-01-21 14:34:35 +0530

    enable yang mode configuration

---
+++ lisp/anupamk/yang-mode-config.el
@@ -0,0 +1,12 @@
+(load-file "$HOME/.emacs.d/lisp/anupamk/yang-mode.el")
+
+
+
+
+
+
+
+
+;;; ----------------------------------------------------------------
+;;; anupamk/yang ends here
+(provide 'anupamk/yang-mode-config)
