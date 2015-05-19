35f47ca112e57c68bb44f550aa05db08e360f90b
Anupam Kapoor <anupam.kapoor@gmail.com>
2015-05-19 10:35:14 +0530

    better multi-term configuration

--- lisp/anupamk/multi-term.el
+++ lisp/anupamk/multi-term.el
@@ -4,39 +4,47 @@
 
 (require 'multi-term)
 
+(global-set-key (kbd "<M-f9>") 'multi-term)
+(global-set-key (kbd "<C-next>") 'multi-term-next)
+(global-set-key (kbd "<C-prior>") 'multi-term-prev)
+
 (setq multi-term-program "/bin/zsh"	; default-shell
+      multi-term-buffer-name "term"
+
       ;; add other settings here...
 
       )
 
 (defcustom term-unbind-key-list
-  '("C-z" "C-x" "C-c" "C-h" "C-y" "<ESC>")
-  "The key list that will need to be unbind."
+  "The key list that will be unbound"
+  '("C-z"
+    "C-x"
+    "C-c"
+    "C-h"
+    "C-y"
+    "<ESC>")
+
   :type 'list
   :group 'multi-term)
- 
-(defcustom term-bind-key-alist
-  '(
-    ("C-c C-c" . term-interrupt-subjob)
-    ("C-p" . previous-line)
-    ("C-n" . next-line)
-    ("C-s" . isearch-forward)
-    ("C-r" . isearch-backward)
-    ("C-m" . term-send-raw)
-    ("M-f" . term-send-forward-word)
-    ("M-b" . term-send-backward-word)
-    ("M-o" . term-send-backspace)
-    ("M-p" . term-send-up)
-    ("M-n" . term-send-down)
-    ("M-M" . term-send-forward-kill-word)
-    ("M-N" . term-send-backward-kill-word)
-    ("M-r" . term-send-reverse-search-history)
-    ("M-," . term-send-input)
-    ("M-." . comint-dynamic-complete))
-  "The key alist that will need to be bind.
-If you do not like default setup, modify it, with (KEY . COMMAND) format."
-  :type 'alist
-  :group 'multi-term)
+
+
+(when (require 'term nil t) ; only if term can be loaded..
+  (setq term-bind-key-alist
+        (list (cons "C-c C-c" 'term-interrupt-subjob)
+              (cons "C-p" 'previous-line)
+              (cons "C-n" 'next-line)
+              (cons "M-f" 'term-send-forward-word)
+              (cons "M-b" 'term-send-backward-word)
+              (cons "C-c C-j" 'term-line-mode)
+              (cons "C-c C-k" 'term-char-mode)
+              (cons "M-DEL" 'term-send-backward-kill-word)
+              (cons "M-d" 'term-send-forward-kill-word)
+              (cons "<C-left>" 'term-send-backward-word)
+              (cons "<C-right>" 'term-send-forward-word)
+              (cons "C-r" 'term-send-reverse-search-history)
+              (cons "M-p" 'term-send-raw-meta)
+              (cons "M-y" 'term-send-raw-meta)
+              (cons "C-y" 'term-send-raw))))
 
 ;; 
 ;; remove some minor annoyances
