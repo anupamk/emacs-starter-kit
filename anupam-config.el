b12758b249788166a84d683d9cfe7be013536c10
Parallel Wireless <parallel@devel-vm.(none)>
2015-10-21 01:08:01 -0500

    slightly better organization of configuration loading on different systems.

--- anupam-config.el
+++ anupam-config.el
@@ -9,8 +9,6 @@
 ;;; generic stuff
 (require 'anupamk/meta)
 
-(unicode-fonts-setup)
-
 ;;; Font setup
 (let ((font "Terminus")
       (size (pcase system-type
@@ -22,18 +20,10 @@
 
 ;;; --------------------------------------------------------------------------------
 ;;; color-theme
-;; (when window-system
-;;   (load-theme 'zenburn t)
-;;   (global-hl-line-mode)
-;;   (load custom-file 'noerror))
-
-;; here we go once again
-;; (require 'color-theme-sanityinc-tomorrow)
-;; (load-theme 'ample t t)
-;; (load-theme 'ample-flat t t)
-(load-theme 'soft-stone)
-;; (load-theme 'ample-light t t)
-
+(when window-system
+  (load-theme 'zenburn t)
+  (global-hl-line-mode)
+  (load custom-file 'noerror))
 
 
 ;; volatile highlights
@@ -47,7 +37,7 @@
   (setq frame-title-format '(buffer-file-name "%f" ("%b"))
 	default-frame-alist '(
 			      ;; basic stuff is same for all frames
-			      (cursor-color . "dark red")
+			      (cursor-color . "gold")
 
 			      ;; add more frame-specific stuff here
 			      )
@@ -55,48 +45,63 @@
 
 (load custom-file 'noerror)
 
-;;; --------------------------------------------------------------------------------
-;;; other useful stuff
-;; (require 'anupamk/modeline)
-(require 'anupamk/clean-aindent-mode-config)
-(require 'anupamk/keyboard)
-(require 'anupamk/revive)
-(require 'anupamk/multi-term)
-(require 'anupamk/occur)
-(require 'anupamk/minibuffer-config)
-(require 'anupamk/magit-config)
-(require 'anupamk/diff-hl-mode-config)
-(require 'anupamk/completion-config)
-(require 'anupamk/rainbow)
-(require 'anupamk/file-handling-config)
-(require 'anupamk/prog-mode-config)
-(require 'anupamk/python-mode-config)
-(require 'anupamk/c-mode-config)
-(require 'anupamk/go-mode-config)
-(require 'anupamk/yang-mode-config)
-(require 'anupamk/xcscope-config)
-(require 'anupamk/window-purpose-config)
-(require 'anupamk/helm)
 
-;; mail+news related settings not on development machines
-(when (not (string= system-name "devel-vm"))
+;;; -------------------------------------------------------------------------------
+;;; finally setup development-vm or local-machine specific configuration
+
+;;
+;; parallel-wireless specific development environment
+;;
+(defun do-pw-devel-config()
+  (message "setting up parallel-wireless configuration")
   (progn
+    (require 'anupamk/clean-aindent-mode-config)
+    (require 'anupamk/keyboard)
+    (require 'anupamk/revive)
+    (require 'anupamk/multi-term)
+    (require 'anupamk/occur)
+    (require 'anupamk/minibuffer-config)
+    (require 'anupamk/diff-hl-mode-config)
+    (require 'anupamk/completion-config)
+    (require 'anupamk/file-handling-config)
+    (require 'anupamk/prog-mode-config)
+    (require 'anupamk/python-mode-config)
+    (require 'anupamk/c-mode-config)
+    (require 'anupamk/yang-mode-config)
+    (require 'anupamk/xcscope-config)
+    (require 'anupamk/window-purpose-config)
+    (require 'anupamk/helm)
+    ))
+
+;; 
+;; standard linux configuration
+;; 
+(defun do-anupamk-linux-config()
+  (message "setting up personal linux configuration")
+  (progn
+    (require 'anupamk/clean-aindent-mode-config)
+    (require 'anupamk/keyboard)
+    (require 'anupamk/revive)
+    (require 'anupamk/multi-term)
+    (require 'anupamk/occur)
+    (require 'anupamk/minibuffer-config)
+    (require 'anupamk/magit-config)
+    (require 'anupamk/diff-hl-mode-config)
+    (require 'anupamk/completion-config)
+    (require 'anupamk/file-handling-config)
+    (require 'anupamk/prog-mode-config)
+    (require 'anupamk/python-mode-config)
+    (require 'anupamk/c-mode-config)
+    (require 'anupamk/go-mode-config)
+    (require 'anupamk/xcscope-config)
+    (require 'anupamk/window-purpose-config)
+    (require 'anupamk/helm)
     (require 'anupamk/spell-checking-config)
     (require 'anupamk/mu4e-config)
     (require 'anupamk/gnus)
-    (require 'anupamk/gnus-bbdb)))
+    (require 'anupamk/gnus-bbdb)
+  ))
 
-
-;; 
-;; NOTE: flyspell mode for programming is not useful at all....
-;; 
-;; ;;
-;; ;; once everything is loaded up, let's enable flyspell checking in
-;; ;; programming mode comments only
-;; ;;
-;; (mapcar (lambda (mode-hook) (add-hook mode-hook 'flyspell-prog-mode))
-;;         '(c-mode-hook
-;; 	  c++-mode-hook
-;; 	  python-mode-hook
-;; 	  emacs-lisp-mode-hook
-;; 	  java-mode-hook))
+(cond ((string= system-name "devel-vm") (do-pw-devel-config))
+      ((string= system-name "fatcat.parallelwireless") (do-anupamk-linux-config))
+)
