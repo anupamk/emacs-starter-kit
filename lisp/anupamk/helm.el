acb7f96b55e583e7328bfc192e364255f967cb1c
Parallel Wireless <parallel@devel-vm.(none)>
2015-10-21 01:10:14 -0500

    slightly better M-x with helm

--- lisp/anupamk/helm.el
+++ lisp/anupamk/helm.el
@@ -11,6 +11,15 @@
 ;;
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 
+;;
+;; this seems to be a better M-x...
+;;
+(global-set-key (kbd "M-x") 'helm-M-x)
+
+;; helm-mini
+(global-set-key (kbd "C-x b") 'helm-mini)
+(setq helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match    t)
 
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)		; rebind tab to run persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)		; make TAB works in terminal
